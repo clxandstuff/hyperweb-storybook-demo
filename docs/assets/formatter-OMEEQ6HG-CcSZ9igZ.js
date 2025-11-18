1b70f469f62b76285a53410bbd418edc8226ea59
Bartek Adamczyk <bartoszadamczykk@gmail.com>
2025-11-18 17:09:46 +0100

    add groups

---
+++ docs/assets/formatter-OMEEQ6HG-CcSZ9igZ.js
@@ -0,0 +1,1 @@
+import{d as t,r as e}from"./WithTooltip-SK46ZJ2J-DGv1nieO.js";import{c as m}from"./iframe-DQW5UkUc.js";import"./preload-helper-PPVm8Dsz.js";import"./react-18-DzfdQ53I.js";import"./jsx-runtime-u17CrQMm.js";var o=m(e(),1),s=(0,o.default)(2)(async(i,r)=>i===!1?r:t(r));export{s as formatter};
